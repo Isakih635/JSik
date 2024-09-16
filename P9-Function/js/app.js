f6be920cd1fcdc5af1a6c282dff3025c0e8ebc62
Isakih635 <isakih635@ihmeducation.se>
2024-09-16 14:57:40 +0200

    changes

---
+++ P9-Function/js/app.js
@@ -0,0 +1,38 @@
+console.log ("stepForward.");
+console.log ("turnRight.");
+console.log ("turnLeft.");
+console.log ("walkThreeSteps.");
+console.log ("turnAround.")
+
+
+function stepForward() {
+  console.log("Move Forward");
+}
+function turnRight(){
+  console.log("Turn Right");
+}
+function turnLeft(){
+  turnRight();
+  turnRight();
+  turnRight();
+}
+function walkThreeSteps(){
+  stepForward();
+  stepForward();
+  stepForward();
+}
+function turnAround(){
+  turnRight();
+  turnRight();
+  }
+
+
+walkThreeSteps()
+turnAround()
+walkThreeSteps()
+turnAround()
+walkThreeSteps()
+turnLeft()
+stepForward()
+turnAround()
+stepForward()
