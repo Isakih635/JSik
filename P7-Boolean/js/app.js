4b35c230d6cc5a4241bb61505ad96d6c96bea8ca
Isakih635 <isakih635@ihmeducation.se>
2024-09-16 13:04:55 +0200

    Change p8

--- P7-Boolean/js/app.js
+++ P7-Boolean/js/app.js
@@ -2,12 +2,32 @@ const birthYear =2010;
 const currentyear =2024
 let ageOfPerson =currentyear - birthYear;
 
-let isChild = ageOfPerson <= 10;
+let isChild = (ageOfPerson <= 0) && (ageOfPerson <=10);
 
-let isTeenager = (ageOfPerson <= 17;) 
+let isTeenager = (ageOfPerson <= 11) && (ageOfPerson <= 17);
 
-let isAdult = ageOfPerson >= 18;
+let isAdult = (ageOfPerson >= 18);
 
 console.log(isChild);
 console.log(isTeenager);
 console.log(isAdult);
+
+let playerTouchGround = false;
+let playerHasJetPack = true;
+
+if (playerTouchGround) {
+  console.log("Player is on the ground.");
+  console.log("Player can jump.");
+}
+
+else if (playerHasJetPack) {
+  console.log("Player can Jet Away");
+}
+
+else {
+console.log("Player is NOT on the ground!");
+console.log("Player CANNOT jump!");
+}
+
+
+
