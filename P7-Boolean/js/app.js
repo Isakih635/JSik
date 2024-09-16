b37dfa6b19c72fc64cddb3c6afba9509d5859faf
Isakih635 <isakih635@ihmeducation.se>
2024-09-16 11:21:39 +0200

    Change P7

---
+++ P7-Boolean/js/app.js
@@ -0,0 +1,13 @@
+const birthYear =2010;
+const currentyear =2024
+let ageOfPerson =currentyear - birthYear;
+
+let isChild = ageOfPerson <= 10;
+
+let isTeenager = (ageOfPerson <= 17;) 
+
+let isAdult = ageOfPerson >= 18;
+
+console.log(isChild);
+console.log(isTeenager);
+console.log(isAdult);
