8216aed61ad89716e3748f06f4eb3009ffb74444
Isakih635 <isakih635@ihmeducation.se>
2024-09-17 15:13:06 +0200

    change

---
+++ Test/js/app.js
@@ -0,0 +1,33 @@
+// Detta är en kommentar...
+/*
+Här är en beskrivning som går över flera rader...
+
+console.log("Hej på Er!");
+console.warn("Akta dig!");
+console.info("Här kommer lite mer information");
+console.error("Nu blev det fel!!!");
+ */
+// Värde är inforationen datat som vi behöver för att kunna arbeta
+console.log("Isabelle");
+
+// Variabler...
+// Lagra värden i namngivna variabler
+// Name är variabel och Isabelle är värdet
+// Isabelle är värdet som vi tilldelar variabeln name
+let name ="Teo";
+
+console.log(name);
+console.log(name);
+
+const birthYear =1992
+const currentYear=2024
+let ageOfTeo =currentYear - birthYear;
+
+
+console.log(ageOfTeo);
+
+
+
+
+
+
