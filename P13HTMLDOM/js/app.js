9d0be20c299fb73959dd4e812bd02ca1dc6a933f
Isakih635 <isakih635@ihmeducation.se>
2024-09-17 14:14:14 +0200

    test

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,2 +1,8 @@
-let textField = document.getElementById("isabelleKihlgren");
-textField.style.color = "pink";
+// Referencing the text field by id
+let textField = document.getElementById('myText');
+
+// Changing the text to your name
+textField.textContent = 'John Doe';  // Byt ut till ditt namn
+
+// Changing the color to your favorite color
+textField.style.color = 'green';  // Byt ut till din favoritfärg
