56ec6f6ced7aa3ed3c54744367dc7f2aa657e781
Isakih635 <isakih635@ihmeducation.se>
2024-09-18 21:01:01 +0200

    click

---
+++ P13/js/app.js
@@ -0,0 +1,18 @@
+const textField0 = document.getElementById("textField0");
+textField0.innerHTML = getRandomInt(20);
+
+const headerText5 = document.getElementById("headerText5");
+headerText5.style.color ="pink";
+
+
+function multiplyBySelf(numberToMultiply) {
+  return numberToMultiply * numberToMultiply;
+}
+function average (firstNumber, secondNumber) {
+  let sumOfNumbers = firstNumber + secondNumber;
+  return sumOfNumbers / 2;
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
