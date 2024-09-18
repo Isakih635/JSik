f1506e7be1f10d639c13dc7c3bf6511fad2a0119
Isakih635 <isakih635@ihmeducation.se>
2024-09-18 13:51:54 +0200

    change

--- P13HTMLDOM/js/app.js
+++ P13HTMLDOM/js/app.js
@@ -1,8 +1,25 @@
-// Referencing the text field by id
-let textField = document.getElementById('myText');
+// Reference "text0" and assign it to the variable called "textField0"
+const textField0 = document.getElementById("text0");
+// TAKE textField0 and change it to "Ben"
+textField0.innerHTML = getRandomInt(20);
 
-// Changing the text to your name
-textField.textContent = 'John Doe';  // Byt ut till ditt namn
+// Reference "header5" and assign it to the variable called "headerText5"
+const headerText5 = document.getElementById("header5");
+// TAKE headerText5 and change it to "THIS IS MA WEBSITE"
+headerText5.innerHTML = "THIS IS MA WEBSITE";
+// TAKE headerText5, access its style, access its color & change it.
+headerText5.style.color = "pink";
 
-// Changing the color to your favorite color
-textField.style.color = 'green';  // Byt ut till din favoritfärg
+
+function multiplyBySelf(numberToMultiply) {
+  return numberToMultiply * numberToMultiply;
+}
+
+function average(firstNumber, secondNumber) {
+  let sumOfNumbers = firstNumber + secondNumber;
+  return sumOfNumbers / 2;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
