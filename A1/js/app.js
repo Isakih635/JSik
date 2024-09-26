a490458a00de30ec8342f0de94cc79134c091b55
Isakih635 <isakih635@ihmeducation.se>
2024-09-26 11:56:49 +0200

    change cookies

--- A1/js/app.js
+++ A1/js/app.js
@@ -65,5 +65,6 @@ function updateStats() {
   computerWinsText.innerText = "Computer Wins: " + computerWins;
   drawsText.innerText = "Draws: " + draws;
 
+  document.cookie = "username=John Doe; expires=Thu, 18 Dec 2025 12:00:00 UTC"
 
 }
