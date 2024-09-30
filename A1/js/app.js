1c3b34d2728a64e611922bc9dd26140f45cbf1cc
Isakih635 <isakih635@ihmeducation.se>
2024-09-30 09:56:59 +0200

    change cookies

--- A1/js/app.js
+++ A1/js/app.js
@@ -17,23 +17,27 @@ let playerWins=0;
 let computerWins=0;
 let draws = 0;
 
+loadGame();
+updateStats();
 
 //Processes
 button0.addEventListener("click", function() {
   getRandomNumberOneToSixForPlayer();
+  getRandomNumberOneToSixForComputer();
   showPlayerRollResult ();
   showComputerRollResult ();
   determineWinner();
   updateStats();
+  saveAsCookie();
+
 });
 
 //Controllers
 function getRandomNumberOneToSixForPlayer() {
   playerRoll = Math.floor(Math.random() * 6) + 1;
-  computerRoll = Math.floor(Math.random() * 6) + 1;
+
 }
   function getRandomNumberOneToSixForComputer() {
-    playerRoll = Math.floor(Math.random() *6) +1;
     computerRoll = Math.floor(Math.random() * 6) + 1;
 }
 
@@ -63,9 +67,36 @@ computerWins++;
 
 // Update statistics
 function updateStats() {
-  playerWinsText.innerText= "Player Wins: " +playerWins;
+  playerWinsText.innerText = "Player Wins: " + playerWins;
   computerWinsText.innerText = "Computer Wins: " + computerWins;
   drawsText.innerText = "Draws: " + draws;
+}
+  //COOKIES
+  function saveAsCookie() {
+    document.cookie = "playerWins" + playerWins + "; expires=Thu, 18 Dec 2025 12:00:00 UTC";
+    document.cookie = "computerWins=" + computerWins + "; expires=Thu, 18 Dec 2025 12:00:00 UTC";
+    document.cookie = "draws=" + draws + "; expires=Thu, 18 Dec 2025 12:00:00 UTC";
+  }
 
+  function loadGame() {
+    playerWins = getCookie("playerWins");
+    computerWins = getCookie("computerWins");
+    draws = getCookie("draws");
+  }
+
+    function getCookie(cname) {
+      let name = cname + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let ca = decodedCookie.split(';');
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+          c = c.substring(1);
+      }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+    }
+    return 0;
 
 }
