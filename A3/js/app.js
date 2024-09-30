30c8fac981bcea925aa924281994ff99145ab7d1
Isakih635 <isakih635@ihmeducation.se>
2024-09-30 12:48:48 +0200

    new project

--- A3/js/app.js
+++ A3/js/app.js
@@ -12,17 +12,46 @@ let computerPickedText = document.getElementById("computerPickedText");
 let playerScoreText = document.getElementById("playerScoreText");
 let computerScoreText = document.getElementById("computerScoreText");
 let drawScoreText = document.getElementById("drawScoreText");
+let evaluationText = document.getElementById("evaluationText");
 
 // DATA
 let playerPicked = 0;
 let computerPicked = 0;
 let draw = 0 ;
+let playerScore = 0;
+let computerScore = 0;
 
 // PROCESSES
+button0.addEventListener("click", function() {
+  getRockForPlayer();
+  getRockForComputer();
+  getScissorForPlayer();
+  getScissorForComputer();
+  getPaperForPlayer();
+  getPaperForComputer();
+
+})
 
 // CONTROLLERS
 function playerPlayed () {
-
+  playerPicked = Math.floor(Math.random() * 3) + 1;
+}
+function computerPlayed () {
+  computerPicked = Math.floor(Math.random() * 3) +1;
+}
+function evaluation (){
+  if (playerPicked > computerPicked) {
+    evaluationText.innerHTML = "Player Wins"
+    playerScore++;
+  }
+    if (playerPicked < computerPicked) {
+      evaluationText.innerHTML = "Computer Wins"
+      computerScore++;
+    }
+      else if ( playerPicked === computerPicked) {
+        evaluationText.innerHTML = "Its a draw!";
+        draw++;
+      }
 }
 
 // VIEWS
